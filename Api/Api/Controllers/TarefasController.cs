20d16d28f3162622993dc119b250cfc36de78284
ricardinho1409 <ricardo00.andrade0@gmail.com>
2022-04-04 21:16:56 -0300

    Api

---
+++ Api/Api/Controllers/TarefasController.cs
@@ -0,0 +1,86 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Api.Controllers
+{
+    public class TarefasController : Controller
+    {
+        Tarefas tarefa;
+        [HttpGet] // Define o tipo de request da api
+        [Route("api/tarefas")] //Define a rota para acessar o endpoint
+        public string Get()
+        {
+            try
+            {
+                tarefa = new Tarefas();
+                var lista = tarefa.Get();
+
+                return JsonConvert.SerializeObject(lista);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;  
+            }
+           
+        }
+        [HttpGet] // Define o tipo de request da api
+        [Route("api/tarefas/{id}")] //Define a rota para acessar o endpoint {Parametro esperado na URL}
+        public string Get(int id)
+        {
+            try
+            {
+                tarefa = new Tarefas();
+                var lista = tarefa.Get(id);
+
+                return JsonConvert.SerializeObject(lista);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+        [HttpPost] // Define o tipo de request da api
+        [Route("api/tarefas/incluir")] //Define a rota para acessar o endpoint
+        public JObject Post([FromBody] Tarefas tarefa)
+        {
+            try
+            {
+                this.tarefa = new Tarefas(0, tarefa.Name, tarefa.Data_Conclusao, tarefa.Concluida);
+                this.tarefa.Save(this.tarefa);
+                var retorno = "{resultado:\"Ok\"}";
+                return JObject.Parse(retorno);
+            }
+            catch (Exception ex)
+            {
+                var retorno = "{resultado:\"" + ex.Message + "\"}";
+                return JObject.Parse(retorno);
+            }
+
+        }
+        [HttpPut] // Define o tipo de request da api
+        [Route("api/tarefas/concluir/{id}")] //Define a rota para acessar o endpoint
+        public JObject Put(int id)
+        {
+            try
+            {
+                this.tarefa = new Tarefas();
+                var t = this.tarefa.Get(id).Find(x=> x.Id.Equals(id));
+                t.Concluida = 'S';
+                this.tarefa.Save(t);
+                var retorno = "{resultado:\"Ok\"}";
+                return JObject.Parse(retorno);
+            }
+            catch (Exception ex)
+            {
+                var retorno = "{resultado:\"" + ex.Message + "\"}";
+                return JObject.Parse(retorno);
+            }
+
+        }
+    }
+}
---
+++ Api/Api/Models/Tarefas.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+namespace Api.Models
+{
+    public class Tarefas
+    {
+        SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Tarefas;Integrated Security=True;Pooling=False");
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? Data_Conclusao { get; set; }
+        public char Concluida { get; set; }
+        public Tarefas() { }
+        public Tarefas(int id, string name, DateTime? data_conclusao, char concluida = 'N')
+        {
+            this.Id = id;   
+            this.Name = name;   
+            this.Data_Conclusao = data_conclusao;
+            this.Concluida = concluida;
+
+        }
+        public bool Save(Tarefas tarefa)
+        {
+            try
+            {
+                var data = Convert.ToDateTime(tarefa.Data_Conclusao);
+                connection.Open();
+                var sql = "";
+                if (tarefa.Id > 0)
+                    sql = "UPDATE Tarefas SET Name = @Name, Data_Conclusao = @Data_Conclusao, Concluida = @Concluida WHERE Id = @Id";
+                else
+                    sql = "INSERT INTO Tarefas (Name, Data_Conclusao, Concluida) VALUES (@Name, @Data_Conclusao, @Concluida)";
+                using(SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    if(tarefa.Id > 0)
+                        cmd.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = tarefa.Id;
+                    cmd.Parameters.Add("@Name", System.Data.SqlDbType.VarChar).Value = tarefa.Name;
+                    cmd.Parameters.Add("@Data_Conclusao", System.Data.SqlDbType.Date).Value = data.ToString("yyyy-MM-dd");
+                    cmd.Parameters.Add("@Concluida", System.Data.SqlDbType.Char).Value = tarefa.Concluida;
+                    int linhas = cmd.ExecuteNonQuery();
+                    connection.Close();
+                    if (linhas > 0)
+                        return true;
+                    else
+                        return false;   
+                }
+            }
+            catch (Exception)
+            {
+                connection.Close();
+                return false;
+            }
+        }
+        public List<Tarefas> Get(int id = 0)
+        {
+            try
+            {
+                connection.Open();
+                var lista = new List<Tarefas>();
+                var sql = "";
+                if (id > 0)
+                    sql = "SELECT Id, Name, Data_Conclusao, Concluida FROM Tarefas WHERE Id = @Id";
+                else
+                    sql = "SELECT Id, Name, Data_Conclusao, Concluida FROM Tarefas WHERE Concluida = 'N' ORDER BY Data_Conclusao";
+                using(SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    if (id > 0)
+                        cmd.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = id;
+                    using(var rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            lista.Add(new Tarefas(Convert.ToInt32(rd[0].ToString()),
+                                rd[1].ToString(),
+                                Convert.ToDateTime(rd[2].ToString()),
+                                Convert.ToChar(rd[3].ToString())));
+                        }
+                        rd.Close();
+                        connection.Close();
+                        return lista;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                connection.Close();
+                return null;
+            }
+        }
+    }
+}
